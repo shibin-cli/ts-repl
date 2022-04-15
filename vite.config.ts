376d95c7766d974e16557af71890f7f61333fc4f
shibin <2263337973@qq.com>
2022-04-15 19:52:44 +0800

    feat: 🎸 init

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/ts-repl/'
+  base:'/ts-repl/',
   resolve:{
     alias: {
       '@': path.join(__dirname, './src')
